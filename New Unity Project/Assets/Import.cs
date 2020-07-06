d32a35f2499f5d81e4729df2b4134b13b85f1506
Juan-Avila92 <juan.avila@3dves.com>
2020-07-06 14:50:35 -0500

    Import - Main script which executes the whole algorithm

---
+++ New Unity Project/Assets/Import.cs
@@ -0,0 +1,52 @@
+﻿using Dummiesman;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Import : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    string objPath = "D:/RevitApiProjects/PrinceGeorge.obj";
+    string error = string.Empty;
+    GameObject loadedObject;
+
+    
+    public void ImportObj()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://3dvs-aras.s3-us-west-2.amazonaws.com/assets/PrinceGeorge.obj");
+        StartCoroutine(WaitAndPrint(www));
+    }
+
+    
+    private IEnumerator WaitAndPrint(UnityWebRequest www)
+    {
+        yield return www.SendWebRequest();
+        
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log("https://3dvs-aras.s3-us-west-2.amazonaws.com/assets/PrinceGeorge.obj");
+            Debug.LogError(www.error);
+            Debug.LogError("HOLAA");
+        }
+        else
+        {
+            var textStream = new MemoryStream(Encoding.UTF8.GetBytes(www.downloadHandler.text));
+            GameObject objModel = new GameObject();
+            objModel = new OBJLoader().Load(textStream);
+
+        }
+    }
+}
